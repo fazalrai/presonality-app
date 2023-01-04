4a0d9ad64087140192d23e4f7276444e67b7c78c
fazalrai <fazal.rai456@gmail.com>
2023-01-05 01:05:49 +0500

    Added Controller and Resource Route

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,11 @@
+class QuestionsController < ApplicationController
+  before_action :get_questions, only: [:index]
+  
+  def index; end
+
+  private
+  def get_questions
+    @questions = Question.all
+  end
+
+end
