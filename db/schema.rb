1a57ad3d1301deffa40cc07b3caafadf9a350ea4
fazalrai <fazal.rai456@gmail.com>
2023-01-05 00:05:49 +0500

    Setup DB Configurations

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_04_164954) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "options", force: :cascade do |t|
+    t.string "text"
+    t.integer "score"
+    t.bigint "question_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_options_on_question_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "options", "questions"
 end
